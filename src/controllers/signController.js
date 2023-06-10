f5eb5f52344a4d3e56ac702185106c04fd5e4635
DavidMorales98 <david.morales.p@usach.cl>
2023-06-09 22:26:04 -0400

    arreglar sign controller abstracción

--- src/controllers/signController.js
+++ src/controllers/signController.js
@@ -1,90 +1,12 @@
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("../utils/jwt")
+const signServices = require("../services/signService");
 
 const signIn = async (req, res) => {
-  try {
-    const params = req.body;
-    if (!params.email || !params.password) {
-      return res.status(400).send({
-        status: "error",
-        message: "Faltan parámetros",
-      });
-    }
-    const existingUser = await User.findOne({
-      email: params.email.toLowerCase(),
-    });
-    if (existingUser) {
-      let isPwd = bcrypt.compareSync(params.password, existingUser.password);
-      if (!isPwd) {
-        return res.status(400).send({
-          status: "error",
-          message: "Contraseña incorrecta",
-        });
-      }
-      const token = jwt.createToken(existingUser)
-      return res.status(200).send({
-        status: "success",
-        message: "Usuario existente",
-        user: {
-          name: existingUser.name,
-          email: existingUser.email,
-          licence: existingUser.licence
-        },
-        token
-      });
-    }
-  } catch (error) {
-    return res.status(500).send({
-      status: "error",
-      message: "Ocurrió un error en el servidor",
-    });
-  }
+  const response = await signServices.signIn(req, res);
+  return res.status(response.status).send(response.send)
 };
 const signUp = async (req, res) => {
-  try {
-    const params = req.body;
-    if (!params.name || !params.email || !params.password) {
-      return res.status(400).send({
-        status: "error",
-        message: "Faltan parámetros",
-      });
-    }
-
-    const existingUser = await User.findOne({
-      email: params.email.toLowerCase(),
-    });
-    if (existingUser) {
-      return res.status(400).send({
-        status: "error",
-        message: "Usuario existente",
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(params.password, 10);
-    params.password = hashedPassword;
-
-    const user = new User(params);
-    const userStored = await user.save();
-    if (!userStored) {
-      return res.status(400).send({
-        status: "error",
-        message: "Error al guardar usuario",
-      });
-    }
-
-    return res.status(200).send({
-      status: "success",
-      user: userStored,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    // Manejar el error de manera apropiada, por ejemplo, enviando una respuesta de error al cliente.
-    return res.status(500).send({
-      status: "error",
-      message: "Ocurrió un error en el servidor",
-    });
-  }
+  const response = await signServices.signUp(req, res);
+  return res.status(response.status).send(response.send)
 };
 
 module.exports = {
--- src/services/signService.js
+++ src/services/signService.js
@@ -1,19 +1,19 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const jwt = require("../utils/jwt")
+const jwt = require("../utils/jwt");
 
 const signIn = async (req, res) => {
   try {
     const params = req.body;
     if (!params.email || !params.password) {
-        return ({
-            status:400,
-            send: {
-                status: "error",
-                message: "Faltan parámetros"
-            },
-            user: null
-        })
+      return {
+        status: 400,
+        send: {
+          status: "error",
+          message: "Faltan parámetros",
+          user: null,
+        },
+      };
     }
     const existingUser = await User.findOne({
       email: params.email.toLowerCase(),
@@ -21,67 +21,82 @@ const signIn = async (req, res) => {
     if (existingUser) {
       let isPwd = bcrypt.compareSync(params.password, existingUser.password);
       if (!isPwd) {
-        return ({
-            status:400,
-            send: {
-                status: "error",
-                message: "Credenciales incorrectas"
-            },
-            user: null
-        })
-      }
-      const token = jwt.createToken(existingUser)
-      return ({
-        status:200,
-        send: {
+        return {
+          status: 400,
+          send: {
             status: "error",
-            message: "Sesión iniciada"
-        },
-        user: {
+            message: "Credenciales incorrectas",
+            user: null,
+          },
+        };
+      }
+      const token = jwt.createToken(existingUser);
+      return {
+        status: 200,
+        send: {
+          status: "success",
+          message: "Sesión iniciada",
+          user: {
             name: existingUser.name,
             email: existingUser.email,
             licence: existingUser.licence,
-            token
+            token,
           },
-    })
+        },
+      };
+    }
+    if (!existingUser) {
+      return {
+        status: 404,
+        send: {
+          status: "error",
+          message: "Credenciales incorrectas",
+          user: {
+            name: existingUser.name,
+            email: existingUser.email,
+            licence: existingUser.licence,
+            token,
+          },
+        },
+      };
     }
   } catch (error) {
-    return ({
-        status:500,
-        send: {
-            status: "error",
-            message: "Ocurrió un error en el servidor"
-        },
-        user: null
-    })
+    return {
+      status: 500,
+      send: {
+        status: "error",
+        message: "Ocurrió un error en el servidor",
+        user: null,
+      },
+    };
   }
 };
 const signUp = async (req, res) => {
   try {
     const params = req.body;
     if (!params.name || !params.email || !params.password) {
-        return ({
-            status:400,
-            send: {
-                status: "error",
-                message: "Faltan parámetros"
-            },
-            user: null
-        })
+      return {
+        status: 400,
+        send: {
+          status: "error",
+          message: "Faltan parámetros",
+          user: null,
+        },
+      };
     }
 
     const existingUser = await User.findOne({
       email: params.email.toLowerCase(),
     });
     if (existingUser) {
-        return ({
-            status:400,
-            send: {
-                status: "error",
-                message: "Usuario ya existe"
-            },
-            user: null
-        })
+      return {
+        status: 400,
+        send: {
+          status: "error",
+          message: "Usuario ya existe",
+          user: null,
+        },
+      };
     }
 
     const hashedPassword = await bcrypt.hash(params.password, 10);
@@ -90,39 +105,39 @@ const signUp = async (req, res) => {
     const user = new User(params);
     const userStored = await user.save();
     if (!userStored) {
-        return ({
-            status:400,
-            send: {
-                status: "error",
-                message: "Error al guardar usuario"
-            },
-            user: null
-        })
+      return {
+        status: 400,
+        send: {
+          status: "error",
+          message: "Error al guardar usuario",
+          user: null,
+        },
+      };
     }
 
-    return ({
-        status:200,
-        send: {
-            status: "success",
-            message: "Usuario guardado"
-        },
-        user: userStored
-    })
+    return {
+      status: 200,
+      send: {
+        status: "success",
+        message: "Usuario guardado",
+        user: userStored,
+      },
+    };
   } catch (error) {
     console.error("Error:", error);
     // Manejar el error de manera apropiada, por ejemplo, enviando una respuesta de error al cliente.
-    return ({
-        status:500,
-        send: {
-            status: "error",
-            message: "Ocurrió un error en el servidor"
-        },
-        user: null
-    })
+    return {
+      status: 500,
+      send: {
+        status: "error",
+        message: "Ocurrió un error en el servidor",
+        user: null,
+      },
+    };
   }
 };
 
 module.exports = {
-    signIn,
-    signUp
-}
+  signIn,
+  signUp,
+};
