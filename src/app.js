71abd8be09c8ce67b493ce20970d2aa374d742b1
Ariel Madariaga Gonzalez <ariel.madariaga@usach.cl>
2023-06-11 19:09:34 -0400

    Se borra .env y se agrega ruta info en app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use("/api/v1",require("./routes/user"))
 app.use("/api/v1",require("./routes/user"))
 app.use("/api/v1",require("./routes/test"))
 app.use("/api/v1",require("./routes/sign"))
+app.use("/api/v1",require("./routes/info"))
 
 
 module.exports = app;
